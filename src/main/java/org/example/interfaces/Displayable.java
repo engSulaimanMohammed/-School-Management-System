bd6ba38e6926708278e0870f920bed8b19791706
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-17 16:18:14 +0400

    Displayable interface,Done

--- src/main/java/org/example/interfaces/Displayable.java
+++ src/main/java/org/example/interfaces/Displayable.java
@@ -1,5 +1,5 @@
 package org.example.interfaces;
 
 public interface Displayable {
-    void displayInfo();  // Display only don't return it.
+    void displayInfo();  // Display only don't return it...
 }
