5ecd86f2b0057fd8e5ba734a5d049fef97ee2318
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-17 16:41:50 +0400

    Add Student entities

--- src/main/java/org/example/entities/Student.java
+++ src/main/java/org/example/entities/Student.java
@@ -1,4 +1,18 @@
 package org.example.entities;
 
-public class Student {
+import java.util.List;
+
+public class Student extends Person {
+    private String GradeLevel;
+    private String RegistrationDate;
+    private double FeeBalance;
+    private List<String> Subjects;
+
+    public Student(String Id, String FirstName, String LastName, String Gender, String PhoneNumber,
+                   String GradeLevel, String RegistrationDate, double FeeBalance  ) {
+
+        super(Id, FirstName, LastName, Gender, PhoneNumber);
+    }
+
+
 }
