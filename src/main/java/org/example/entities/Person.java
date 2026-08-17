e8e5f54b7e9e9be02d964744d196a7ff9fd43cda
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-17 16:17:04 +0400

    Class Person Done

--- src/main/java/org/example/entities/Person.java
+++ src/main/java/org/example/entities/Person.java
@@ -1,18 +1,102 @@
 package org.example.entities;
 
-public class Person {
-    private String ID;
+import org.example.interfaces.Displayable;
+
+import java.util.Objects;
+
+public class Person implements Displayable {
+    private String Id;
     private String FirstName;
     private String LastName;
     private String Gender;
     private String PhoneNumber;
-    private String Email;
-    private String DateOfBrith;
+
+    public Person(String Id, String FirstName, String LastName,
+                  String Gender, String PhoneNumber) {
+        setID(Id);
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Gender = Gender;
+        this.PhoneNumber = PhoneNumber;
+
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public void setID( String Id) {
+        if (utils.HelperUtils.isEmpty(id)) {
+            IO.println("Id cannot be empty. Keeping the old id.");
+            return;
+        }
+        this.Id = Id;
+    }
 
 
+    @Override
+    public void displayInfo() {  // From the interfaces
+        IO.println("Person: " + FirstName + " " + LastName + "(ID: " + Id + ")");
 
+    }
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "Id='" + Id + '\'' +
+                ", FirstName='" + FirstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", Gender='" + Gender + '\'' +
+                ", PhoneNumber='" + PhoneNumber + '\'' +
+                '}';
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(Id, person.Id);
+    }
 
+    /*@Override
+    public int hashCode() {
+        return Objects.hashCode(Id);
+    }*/
 
 }
--- src/main/java/org/example/interfaces/Displayable.java
+++ src/main/java/org/example/interfaces/Displayable.java
@@ -1,4 +1,5 @@
 package org.example.interfaces;
 
 public interface Displayable {
+    void displayInfo();  // Display only don't return it.
 }
