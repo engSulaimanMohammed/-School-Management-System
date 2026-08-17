bf6eaf82a4b2c6517c457a901cb533c91a2d7ecf
Suliman Al-Hinai <suliman9mohammed@gmail.com>
2026-08-17 10:32:37 +0400

    Add class person

--- src/main/java/org/example/entities/Person.java
+++ src/main/java/org/example/entities/Person.java
@@ -1,4 +1,18 @@
 package org.example.entities;
 
 public class Person {
+    private String ID;
+    private String FirstName;
+    private String LastName;
+    private String Gender;
+    private String PhoneNumber;
+    private String Email;
+    private String DateOfBrith;
+
+
+
+
+
+
+
 }
